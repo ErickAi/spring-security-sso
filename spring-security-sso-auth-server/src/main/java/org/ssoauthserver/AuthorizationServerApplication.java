6effd235e4b79dd68a4356cdfa6c48436e2ef8e1
Erick Aytkulov <aytkulov@iac.spb.ru>
2019-09-20 12:34:25 +0300

    refactor: auth server

--- spring-security-sso-auth-server/src/main/java/org/ssoauthserver/AuthorizationServerApplication.java
+++ spring-security-sso-auth-server/src/main/java/org/ssoauthserver/AuthorizationServerApplication.java
@@ -1,4 +1,4 @@
-package org.ssoauthserver.config;
+package org.ssoauthserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-security-sso-auth-server/src/test/java/org/securityclient/SpringContextIntegrationTest.java
+++ spring-security-sso-auth-server/src/test/java/org/securityclient/SpringContextIntegrationTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.ssoauthserver.config.AuthorizationServerApplication;
+import org.ssoauthserver.AuthorizationServerApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = AuthorizationServerApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
